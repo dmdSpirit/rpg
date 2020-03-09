322d941f4ada3af15a820269a70de82c7c53eedf
Stas Volkov <doomedspirit.prog@gmail.com>
2020-03-09 17:45:13 +0200

    Started working on skill system

---
+++ Assets/Scripts/Skill.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public enum SkillType
+{
+    Moving,
+    Target,
+    AOE
+}
+
+public class Skill : ScriptableObject
+{
+    public string name;
+    public SkillType type;
+    public Sprite image;
+    public string description;
+}
---
+++ Assets/Scripts/SkillButton.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class SkillButton : MonoBehaviour
+{
+    [SerializeField] private Image image;
+
+    private Skill skill;
+
+    public void Initialize(Skill skill)
+    {
+        this.skill = skill;
+    }
+}
